45e2fade754f8e70bf3414c4c03f9ba655a08a8e
Administrator <Administrator@WIN-1BDR0PFIKRD>
2022-11-08 11:15:43 +0700

    3rd lesson

---
+++ bootcamp/src/main/java/com/bcafinance/training/ThirdLesson.java
@@ -0,0 +1,13 @@
+package com.bcafinance.training;
+/*
+@Author Administrator a.k.a. Bambang
+calon menantu idaman
+created with Eclipse version 
+Created on 08-11-2022 11:15:26
+Last Modified on 08-11-2022 11:15:26
+Version 1.0
+*/
+
+public class ThirdLesson {
+
+}
