ff0adbe999372bc662e9b641c10a35cb789c188a
Administrator <Administrator@WIN-1BDR0PFIKRD>
2022-11-08 10:21:20 +0700

    audit trail

--- src/main/java/com/bcafinance/training/day2/FirstLesson.java
+++ src/main/java/com/bcafinance/training/day2/FirstLesson.java
@@ -1,4 +1,12 @@
 package com.bcafinance.training.day2;
+/*
+@Author Administrator a.k.a. Bambang
+calon menantu idaman
+created with Eclipse version 
+Created on 08-11-2022 10:20:15
+Last Modified on 08-11-2022 10:20:15
+Version 1.0
+*/
 
 public class FirstLesson {
 
